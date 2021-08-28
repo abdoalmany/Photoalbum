e43e2f867beb92825dc36699832005fe5354eb34
abdoalmany <3almany@gmail.com>
2021-08-28 13:06:52 +0200

    migrations

--- photos/models.py
+++ photos/models.py
@@ -8,8 +8,9 @@ class Category(models.Model):
         return self.name
 
 class Photo(models.Model):
-    category = models.ForeignKey(Category , on_delete = models.SET_NULL ,null =True)
-    description = models.TextField(max_length=500 , null=False , blank = False)
+    category = models.ForeignKey(Category , on_delete = models.SET_NULL ,null =True , blank =True )
     image = models.ImageField(null = False , blank = False)
+    description = models.TextField(max_length=500 , null=False , blank = False)
+
     def __str__(self):
         return self.description
