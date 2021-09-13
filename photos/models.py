ef4ba194a3edb8f35a3bc6e692fa7dad63d54df4
abdoalmany <3almany@gmail.com>
2021-09-13 16:30:22 +0200

    This is the full project

--- photos/models.py
+++ photos/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 class Photo(models.Model):
     category = models.ForeignKey(Category , on_delete = models.SET_NULL ,null =True , blank =True )
     image = models.ImageField(null = False , blank = False)
-    description = models.TextField(max_length=500 , null=False , blank = False)
+    description = models.TextField(max_length=500 , null=False , blank = False )
 
     def __str__(self):
         return self.description
